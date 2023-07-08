73a5edb86974c5884dabe5bc3e508b702ba17e08
Mason Stooksbury <masonstooksbury@gmail.com>
2023-07-08 14:53:39 -0400

    Added second player, set moving camera logic

--- Shared/Menu/Game.gd
+++ Shared/Menu/Game.gd
@@ -1,18 +1,23 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var player1_pos = $Player.global_transform.origin
-	var stuff = player1_pos.distance_to($Player2.global_transform.origin)
-	print(stuff)
-#	$Camera2D.global_transform = $Player.global_transform
+var zoom_max = 0.5
+var zoom_min = 0.7
+var distance_min = 10
+var distance_max = 600
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	var player1_pos = $Player.global_transform.origin
-#	player1_pos.distance_to($Player2.global_transform.origin)
-#
-#
-#	$Camera2D.global_transform = $Player.global_transform
+func _process(delta):
+	var new_vector = ($Player.global_transform.origin + $Player2.global_transform.origin)/2
+	var distance = ($Player.global_transform.origin).distance_to($Player2.global_transform.origin)
+
+	handle_zoom(distance)
+	$Camera2D.global_transform.origin = new_vector
+
+
+func handle_zoom(distance):
+	var zoom_percent = distance / (distance_max - distance_min)
+	zoom_percent = zoom_min if zoom_percent > zoom_min else zoom_percent
+	zoom_percent = zoom_max if zoom_percent < zoom_max else zoom_percent
+	
+	$Camera2D.zoom = Vector2(1-zoom_percent, 1-zoom_percent)
